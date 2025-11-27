85842087b3f339554834bd7c05b7f92f6873bb34
Krivega <mr.krivega@gmail.com>
2025-11-27 12:55:21 +0300

    chore: migrate to release-it

---
+++ .release-it.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'release-it';
+
+export default {
+  git: {
+    requireBranch: 'main',
+    commitMessage: 'chore: release v${version}',
+    commit: true,
+    tag: true,
+    push: true,
+  },
+  npm: {
+    publish: true,
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: {
+        name: 'angular',
+      },
+      infile: 'CHANGELOG.md',
+    },
+  },
+  hooks: {
+    'before:init': ['yarn lint'],
+    // 'after:conventional-changelog:bump': 'prettier --write "CHANGELOG.md"',
+    'after:bump': 'yarn build',
+    'after:release': 'git push origin HEAD --follow-tags',
+  },
+} satisfies Config;
