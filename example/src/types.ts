8d51f04d609540f29034a123e796905847fdffd2
Krivega <mr.krivega@gmail.com>
2026-02-24 12:05:30 +0300

    chore:  update dependencies

--- example/src/types.ts
+++ example/src/types.ts
@@ -1,3 +1,25 @@
 // demo type file for type-only imports
 export type Numeric = number;
 export type UserId = string & { readonly brand: unique symbol };
+
+export type TGetReleaseNotes = ({ from, to }?: { from?: number; to?: number }) => Promise<string>;
+
+export type TTestIds = {
+  list: string;
+  listItem: string;
+  edit: string;
+  addConnectionProfile: string;
+  statusIcon: string;
+};
+
+export type TParams = {
+  testIds: TTestIds;
+  toAddSettingsConnectionProfile: ({ isRegisteredUser }?: { isRegisteredUser?: boolean }) => void;
+  toEditSettingsConnectionProfile: ({
+    uuid,
+    isRegisteredUser,
+  }: {
+    uuid: string;
+    isRegisteredUser?: boolean;
+  }) => void;
+};
